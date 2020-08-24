a6a3e1155de4cf23cf03cb99de4c2709a795e236
agungsafdaa <54742335+agungsafdaa@users.noreply.github.com>
2020-08-24 19:46:07 +0700

    form todo-header

--- src/App.js
+++ src/App.js
@@ -2,54 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "../component/Paper";
+import Header from "../component/Header";
+import TodoForm from "../component/TodoForm";
+import Todos from "../component/Todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn">Add</button>
-        <h1 className="header-tittle">Todo list</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn">Add</button>
-        </form>
-      </section>
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning react</span>
-        </div>
-      </section>
+      <Header></Header>
+      <TodoForm></TodoForm>
+      <Todos></Todos>
     </Paper>
   );
 }
