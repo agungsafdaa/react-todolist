d6315d9ddda8bbb9103ff2aa6cc387776200fab2
agungsafdaa <54742335+agungsafdaa@users.noreply.github.com>
2020-09-06 15:28:36 +0700

    PropTypes ToDo

--- component/TodoForm.jsx
+++ component/TodoForm.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!value) {
+      alert("no blank todo");
+      return;
+    }
+    addTodo(value);
+    alert(value);
+    setValue("");
+  };
+  console.log("value", value);
   return (
     <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn">Add</button>
       </form>
     </section>
   );
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired
 };
 
 export default TodoForm;
