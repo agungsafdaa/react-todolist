a7522b7db4f3f7bd9bc1928b6ddb8feb161130c7
agungsafdaa <54742335+agungsafdaa@users.noreply.github.com>
2020-09-10 13:34:05 +0700

    complete todo function

--- component/Todos.jsx
+++ component/Todos.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import Todo from "./Todo";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, completeTodo }) => {
   return (
     <section ClassName="todos">
-      {todos.map((todo) => {
-        return <Todo text={todo.text} />;
+      {todos.map((todo, index) => {
+        return (
+          <Todo
+            key={index}
+            text={todo.text}
+            isCompleted={todo.isCompleted}
+            completeTodo={completeTodo}
+            index={index}
+          />
+        );
       })}
     </section>
   );
@@ -17,7 +25,8 @@ Todos.propTypes = {
     PropTypes.shape({
       text: PropTypes.string
     })
-  )
+  ),
+  completeTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
--- pages/TodoList.jsx
+++ pages/TodoList.jsx
@@ -6,24 +6,28 @@ import Todos from "../component/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "Learning react!" },
-    { text: "Learning react Hooks!" },
-    { text: "Learning Styling in react!" }
+    { text: "Learning react!", isCompleted: false },
+    { text: "Learning react Hooks!", isCompleted: false },
+    { text: "Learning Styling in react!", isCompleted: false }
   ]);
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
     setTodos(addedTodo);
   };
-
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
+    setTodos(addedTodo);
+  };
   const showAddToggle = () => setShowAdd(!showAdd);
-  console.log("Showadd", showAdd);
+  console.log("todos", todos);
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
